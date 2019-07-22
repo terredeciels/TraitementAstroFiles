0e2e7f5f0f7ac9cf7c9d1e84d0faee56bb5c9d51
Gilles <chekunive@gmail.com>
2019-07-22 16:42:25 +0200

    filtre

--- src/main/java/main/Filtre.java
+++ src/main/java/main/Filtre.java
@@ -19,26 +19,5 @@ class Filtre {
         return !(altitude > altmax) && !(altitude < altmin)
                 && !(azimuth > azmax) && !(azimuth < azmin);
     }
-//
-//    private void traitement(double[] utDateNow, int pass) throws IOException {
-//        lstNow = SkyAlgorithms.CalcLST((int) utDateNow[0], (int) utDateNow[1], (int) utDateNow[2], utDateNow[3], longitude, leapSec);
-//
-//        CSVWriter writer = new CSVWriter(new FileWriter(pathFichierResult + nomFichierSortie + pass + ".csv"), ',');
-//        CSVReader reader = new CSVReader(new FileReader(pathFichierEntree));
-//        String[] nextLine;
-//        reader.readNext();//pass first line
-//        outPutFile[0] = "name";
-//        outPutFile[1] = "ra";
-//        outPutFile[2] = "dec";
-//        outPutFile[3] = "Const";
-//        outPutFile[4] = "alt";
-//        outPutFile[5] = "az";
-//        writer.writeNext(outPutFile);
-//        while ((nextLine = reader.readNext()) != null) {
-//            convertToAzH(nextLine);
-//            writer.writeNext(outPutFile);
-//        }
-//        writer.close();
-//    }
 
 }
--- src/main/java/main/TraitementAstroFiles.java
+++ src/main/java/main/TraitementAstroFiles.java
@@ -10,19 +10,21 @@ import java.io.IOException;
 
 public class TraitementAstroFiles extends ETraitementAF {
 
-    // private final Filtre filtre;
+    private final Filtre filtre;
     private double lstNow;
     private String[] outPutFile;
+    private String[] temPoutPutFile;
 
     private TraitementAstroFiles() throws IOException {
         outPutFile = new String[10];
+        temPoutPutFile = new String[10];
 
         double[] utDateNow0 = getDateTime(day_of_month0, month0, year0,
                 hours0, minutes0, seconds0, milliseconds0);
         double[] utDateNow1 = getDateTime(day_of_month1, month1, year1,
                 hours1, minutes1, seconds1, milliseconds1);
 
-        // filtre = new Filtre(20, 70, -30, 30);
+        filtre = new Filtre(20, 70, -30, 30);
         traitement(utDateNow0, 0);
         //  traitement(utDateNow1, 1);
 
@@ -61,20 +63,20 @@ public class TraitementAstroFiles extends ETraitementAF {
 
     private void convertToAzH(String[] nextLine) {
         double ra, dec;
-        outPutFile[0] = nextLine[0];//name
+        temPoutPutFile[0] = nextLine[0];//name
 
         String raS = nextLine[1];
         ra = 15 * ETraitementAF.sexToDec(raS);
-        outPutFile[1] = Double.toString(ra);// ra
+        temPoutPutFile[1] = Double.toString(ra);// ra
         String decS = nextLine[2];
         dec = ETraitementAF.sexToDec(decS);
-        outPutFile[2] = Double.toString(dec);// dec
+        temPoutPutFile[2] = Double.toString(dec);// dec
 
-        outPutFile[3] = nextLine[3];//Const
-        outPutFile[4] = nextLine[4];//VarType
-        outPutFile[5] = nextLine[5];//MinMag
-        outPutFile[6] = nextLine[6];//MaxMag
-        outPutFile[7] = nextLine[7];//Period
+        temPoutPutFile[3] = nextLine[3];//Const
+        temPoutPutFile[4] = nextLine[4];//VarType
+        temPoutPutFile[5] = nextLine[5];//MinMag
+        temPoutPutFile[6] = nextLine[6];//MaxMag
+        temPoutPutFile[7] = nextLine[7];//Period
 
 //        try {
 ////            ra = Double.parseDouble(raS);
@@ -89,11 +91,17 @@ public class TraitementAstroFiles extends ETraitementAF {
         double ha = lstNow - ra;
         double[] altaz = SkyAlgorithms.EquatorialToHorizontal(ha, dec, latitude);
         double altitude = altaz[0];
-        outPutFile[8] = Double.toString(altitude);
+        temPoutPutFile[8] = Double.toString(altitude);
         double azimuth = altaz[1];
-        outPutFile[9] = Double.toString(azimuth);
+        temPoutPutFile[9] = Double.toString(azimuth);
 
+        validate(altitude, azimuth);
+    }
 
+    private void validate(double altitude, double azimuth) {
+        boolean valid = filtre.exe(altitude, azimuth);
+        if (valid)
+            System.arraycopy(temPoutPutFile, 0, outPutFile, 0, temPoutPutFile.length);
     }
 
 }
